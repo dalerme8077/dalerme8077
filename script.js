44dc1aeee0ead5502776c5c26700915f65d209af
dalerme8077 <muhammadalimuhsinov8@gmail.com>
2025-05-13 17:22:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-const androidBtn = document.getElementById('androidBtn');
-const iosBtn = document.getElementById('iosBtn');
-const androidApps = document.getElementById('androidApps');
-const iosApps = document.getElementById('iosApps');
+function showAndroid() {
+  document.getElementById('android').classList.add('active');
+  document.getElementById('ios').classList.remove('active');
+  document.getElementById('android-tab').classList.add('active');
+  document.getElementById('ios-tab').classList.remove('active');
+}
 
-androidBtn.addEventListener('click', () => {
-    androidApps.classList.remove('hidden');
-    iosApps.classList.add('hidden');
-    androidBtn.classList.add('active');
-    iosBtn.classList.remove('active');
-});
-
-iosBtn.addEventListener('click', () => {
-    iosApps.classList.remove('hidden');
-    androidApps.classList.add('hidden');
-    iosBtn.classList.add('active');
-    androidBtn.classList.remove('active');
-});
+function showIos() {
+  document.getElementById('ios').classList.add('active');
+  document.getElementById('android').classList.remove('active');
+  document.getElementById('ios-tab').classList.add('active');
+  document.getElementById('android-tab').classList.remove('active');
+}
